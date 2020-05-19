e8e673a8164cfefc83d906a814303b2bc4628edc
Ryan Duff <ryanduff613@gmail.com>
2020-05-19 10:53:12 -0700

    create test for .clear on artist class

--- spec/artist_spec.rb
+++ spec/artist_spec.rb
@@ -8,6 +8,15 @@ describe '#Artist' do
     end
   end
 
-
+  describe('.clear') do
+    it("clears all artists") do
+      artist = Artist.new({:name => "John Coltrane", :id => nil})
+      artist.save()
+      artist2 = Artist.new({:name => "Kanye", :id => nil})
+      artist2.save()
+      Artist.clear
+      expect(Artist.all).to(eq([]))
+    end
+  end
 
 end
