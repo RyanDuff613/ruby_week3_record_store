fd935d462f617b11ad18f765ceec4555222b70ea
Ryan Duff <ryanduff613@gmail.com>
2020-05-19 10:28:07 -0700

    create .all method on artist class, tests passing

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,4 +1,23 @@
 class Artist
+  attr_accessor :name
+  attr_reader :id
+
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @id = attributes.fetch(:id)
+  end
+
+  def self.all
+    returned_artists = DB.exec("SELECT * FROM albums;")
+    artists = []
+    returned_artists.each() do |artist|
+      name = artist.fetch("name")
+      id = artist.fetch("id").to_i
+      albums.push(Artist.new({:name => name, :id => id}))
+    end
+    artists
+  end
+
 
 end
   
